e51a328c1cd37cdfc7d8a39ef0d3f439179fed77
Son Tran-Nguyen <github@sntran.com>
2021-11-21 21:40:53 -0600

    refactor: LiveMap is the component itself

--- lib/live_map.ex
+++ lib/live_map.ex
@@ -3,4 +3,20 @@ defmodule LiveMap do
   @moduledoc """
   #{File.read!(@external_resource)}
   """
+
+  use Phoenix.LiveComponent
+
+  @impl Phoenix.LiveComponent
+  @spec mount(Phoenix.LiveView.Socket.t()) :: {:ok, Phoenix.LiveView.Socket.t()}
+  def mount(socket) do
+    {:ok,
+     socket
+     |> assign_new(:width, fn -> 300 end)
+     |> assign_new(:height, fn -> 150 end)
+     |> assign_new(:title, fn -> "" end)
+    }
+  end
+
+  # @callback render/1 is handled by `Phoenix.LiveView.Renderer.before_compile`
+  # by looking for a ".html" file with the same name as this module.
 end
--- test/live_map_test.exs
+++ test/live_map_test.exs
@@ -1,4 +1,36 @@
 defmodule LiveMapTest do
   use ExUnit.Case
   doctest LiveMap
+
+  use ExUnit.Case
+  import Phoenix.LiveViewTest
+
+  describe "container" do
+
+    test "renders component as an SVG" do
+      component = render_component(LiveMap, id: "live-map")
+      assert component =~ "</svg>"
+    end
+
+    test "contains a <style> descendant" do
+      component = render_component(LiveMap, id: "live-map")
+      assert component =~ "</style>"
+    end
+
+    test "supports setting width" do
+      component = render_component(LiveMap, id: "live-map", width: 300)
+      assert component =~ "width=\"300\""
+    end
+
+    test "supports setting height" do
+      component = render_component(LiveMap, id: "live-map", height: 150)
+      assert component =~ "height=\"150\""
+    end
+
+    test "supports setting title as <title>" do
+      component = render_component(LiveMap, id: "live-map", title: "Awesome Live Map")
+      assert component =~ "<title>Awesome Live Map</title>"
+    end
+
+  end
 end
