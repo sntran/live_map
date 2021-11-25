b8c137e945fb9d34f8272e057951198cef2c5886
Son Tran-Nguyen <github@sntran.com>
2021-11-25 15:24:27 -0600

    feat: render tile image for each tile

--- lib/live_map.ex
+++ lib/live_map.ex
@@ -4,16 +4,40 @@ defmodule LiveMap do
   #{File.read!(@external_resource)}
   """
 
+  require Logger
+  alias LiveMap.Tile
+
   use Phoenix.LiveComponent
 
   @impl Phoenix.LiveComponent
   @spec mount(Phoenix.LiveView.Socket.t()) :: {:ok, Phoenix.LiveView.Socket.t()}
   def mount(socket) do
     {:ok,
-     socket
-     |> assign_new(:width, fn -> 300 end)
-     |> assign_new(:height, fn -> 150 end)
-     |> assign_new(:title, fn -> "" end)
+      socket
+      |> assign_new(:width, fn -> 300 end)
+      |> assign_new(:height, fn -> 150 end)
+      |> assign_new(:title, fn -> "" end)
+    }
+  end
+
+  @impl Phoenix.LiveComponent
+  @spec update(Phoenix.LiveView.Socket.assigns(), Phoenix.LiveView.Socket.t()) ::
+          {:ok, Phoenix.LiveView.Socket.t()}
+  def update(assigns, socket) do
+    width = assigns[:width] || socket.assigns[:width]
+    height = assigns[:height] || socket.assigns[:height]
+    latitude = assigns[:latitude] || socket.assigns[:latitude] || 0
+    longitude = assigns[:longitude] || socket.assigns[:longitude] || 0
+    zoom = assigns[:zoom] || socket.assigns[:zoom] || 0
+
+    center = Tile.at(latitude, longitude, zoom)
+    tiles = Tile.map(center, width, height)
+
+    {:ok,
+      socket
+      |> assign(assigns)
+      |> assign(:zoom, zoom)
+      |> assign(:tiles, tiles)
     }
   end
 
--- test/live_map_test.exs
+++ test/live_map_test.exs
@@ -1,36 +1,56 @@
 defmodule LiveMapTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest LiveMap
 
-  use ExUnit.Case
   import Phoenix.LiveViewTest
 
   describe "container" do
 
     test "renders component as an SVG" do
-      component = render_component(LiveMap, id: "live-map")
-      assert component =~ "</svg>"
+      assert component() =~ "</svg>"
     end
 
     test "contains a <style> descendant" do
-      component = render_component(LiveMap, id: "live-map")
-      assert component =~ "</style>"
+      assert component() =~ "</style>"
     end
 
     test "supports setting width" do
-      component = render_component(LiveMap, id: "live-map", width: 300)
-      assert component =~ "width=\"300\""
+      assert component(width: 300) =~ "width=\"300\""
     end
 
     test "supports setting height" do
-      component = render_component(LiveMap, id: "live-map", height: 150)
-      assert component =~ "height=\"150\""
+      assert component(height: 150) =~ "height=\"150\""
     end
 
     test "supports setting title as <title>" do
-      component = render_component(LiveMap, id: "live-map", title: "Awesome Live Map")
-      assert component =~ "<title>Awesome Live Map</title>"
+      assert component(title: "Awesome Live Map") =~ "<title>Awesome Live Map</title>"
     end
 
+  end
+
+  describe "tiles" do
+
+    test "should have 1 tile covering the whole wold at zoom 0" do
+      rendered = component(zoom: 0)
+      {:ok, document} = Floki.parse_document(rendered)
+      assert [tile] = Floki.find(document, "image")
+      assert Floki.attribute(tile, "x") === ["0"]
+      assert Floki.attribute(tile, "y") === ["0"]
+      assert Floki.attribute(tile, "width") === ["256px"]
+      assert Floki.attribute(tile, "height") === ["256px"]
+    end
+
+    test "should have 4 tiles at zoom 1" do
+      rendered = component(zoom: 1)
+      {:ok, document} = Floki.parse_document(rendered)
+      tiles = Floki.find(document, "image")
+      assert length(tiles) === 4
+    end
+
+  end
+
+  defp component(assigns \\ []) do
+    assigns = Keyword.merge([id: "live-map"], assigns)
+    render_component(LiveMap, assigns)
   end
 end
